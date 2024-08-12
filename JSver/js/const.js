6d9b94c3c405671cd518f8504929e69244f6a506
“domekenshi” <kfprog5136@gmail.com>
2024-08-12 15:56:25 +0900

    リファクタリング

--- JSver/js/const.js
+++ JSver/js/const.js
@@ -1,2 +1,4 @@
 export const API_URL =
   "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+export const BASE_TWITTER_URL = "https://twitter.com/intent/tweet?text=";
--- JSver/js/script.js
+++ JSver/js/script.js
@@ -1,4 +1,4 @@
-import { API_URL } from "./const.js";
+import { API_URL, BASE_TWITTER_URL } from "./const.js";
 const quoteContainer = document.getElementById("quote-container");
 const quoteTxt = document.getElementById("quote");
 const authorTxt = document.getElementById("author");
@@ -7,30 +7,8 @@ const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
-/**
- * 新規引用文生成
- */
-function newQuote() {
-  // Pick a random quote from apiQuotes array ランダムに引用文配列から引用を取得
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // Check if Author field is blank and replace it with 'Unknown'
-  // 著者がnullかどうかをチェックする
-  if (!quote.author) {
-    authorTxt.textContent = "不明";
-  } else {
-    authorTxt.textContent = quote.author;
-  }
-  // Check Quote length to determine sytyling
-  // 引用文の長さに応じてスタイルを変更する
-  if (quote.text.length > 50) {
-    console.log("長い");
-    quoteTxt.classList.add("long-quote");
-  } else {
-    console.log("短い");
-    quoteTxt.classList.remove("long-quote");
-  }
-  quoteTxt.textContent = quote.text;
-}
+const UNKNOWN = "不明";
+const QUOTE_STYLE_CHANGE_NUM = 50;
 
 /**
  * 引用文取得
@@ -49,14 +27,38 @@ async function getQuotes() {
 }
 
 /**
+ * 新規引用文生成
+ */
+function newQuote() {
+  // Pick a random quote from apiQuotes array ランダムに引用文配列から引用を取得
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  // 著者がnullかどうかをチェックする
+  if (!quote.author) {
+    authorTxt.textContent = UNKNOWN;
+  } else {
+    authorTxt.textContent = quote.author;
+  }
+  // Check Quote length to determine sytyling
+  // 引用文の長さに応じてスタイルを変更する
+  if (quote.text.length > QUOTE_STYLE_CHANGE_NUM) {
+    quoteTxt.classList.add("long-quote");
+  } else {
+    quoteTxt.classList.remove("long-quote");
+  }
+  quoteTxt.textContent = quote.text;
+}
+
+/**
  * 引用文ツイート
  */
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteTxt.textContent} - ${authorTxt.textContent}`;
+  const twitterUrl = `${BASE_TWITTER_URL}${quoteTxt.textContent} - ${authorTxt.textContent}`;
   window.open(twitterUrl, "_blank");
 }
 
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
+
 // 初回画面起動時に呼び出す
 getQuotes();
